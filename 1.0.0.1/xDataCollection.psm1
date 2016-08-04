395eb36d6851afa339763ddef11f3882826f4ba2
voleresistor <voleresistor@gmail.com>
2016-08-04 15:32:04 -0500

    Add Get-BLStatus
    Monitor Bitlocker progress on remote system drive.

--- 1.0.0.1/xDataCollection.psm1
+++ 1.0.0.1/xDataCollection.psm1
@@ -18,6 +18,8 @@
         07/29/16 - v1.0.0.4
             Add Set-FutureRestart
             Add Get-LocalTime
+        08/04/16
+            Add Get-BLStatus
 #>
 
 #region Get-MemoryStats
@@ -1020,6 +1022,51 @@ function Set-FutureRestart
         $STAction = New-ScheduledTaskAction -Execute $ExePath -Argument $ActionArg
         $STTrigger = New-ScheduledTaskTrigger -Once -At $RunTime
         Register-ScheduledTask -TaskName $TaskName -Description $TaskDesc -User 'SYSTEM' -Action $STAction -Trigger $STTrigger
+    }
+}
+#endregion
+#region Get-BLStatus
+Function Get-BLStatus
+{
+    <#
+    .Synopsis
+    Monitor encryption status on remote computers.
+    
+    .Description
+    Using a remote session, get status of system drive encryption on remote computers.
+    
+    .Parameter ComputerName
+    The remote computer to target for monitoring.
+    
+    .Example
+    Get-BLStatus -ComputerName pc001.contoso.com
+    
+    Get encryption progress updates until encryption is completed.
+    #>
+    param
+    (
+        [string]$ComputerName = $env:localhost
+    )
+    
+    try
+    {
+        # Generate a session to run the remote command in
+        $BLMonSession = New-PSSession -ComputerName $ComputerName
+        
+        do
+        {
+            $Volume = (Invoke-Command -Session $BLMonSession -ScriptBlock {Get-BitLockerVolume -MountPoint $env:SystemDrive})
+            Write-Progress -Activity "Encrypting volume $($Volume.MountPoint) on $ComputerName" -Status "Encryption Progress - $($Volume.EncryptionPercentage)%" -PercentComplete $Volume.EncryptionPercentage
+            Start-Sleep -Seconds 1
+        }
+        until ($Volume.VolumeStatus -eq 'FullyEncrypted')
+    }
+    
+    # We want to kill the previously generated session when we're done
+    # no matter what happened in between
+    finally
+    {
+        Remove-PSSession -Id $($a.Id)
     }
 }
 #endregion
