39113d83a69555fc7f2b58f095a732df70bf5d2f
voleresistor <voleresistor@gmail.com>
2016-08-30 11:44:02 -0500

    Added Invoke-PowerShell
    PowerShell wrapper for robocopy

--- 1.0.0.1/xDataCollection.psm1
+++ 1.0.0.1/xDataCollection.psm1
@@ -1070,3 +1070,305 @@ Function Get-BLStatus
     }
 }
 #endregion
+#region Invoke-RoboCopy
+function Invoke-Robocopy
+{
+    <#
+    .Synopsis
+    PowerShell wrapper for RoboCopy.
+    
+    .Description
+    Provides switches and simple syntax including Intellisense to simplify PowerShell usage.
+    
+    .Parameter Source
+    The source location.
+    
+    .Parameter Destination
+    The destination location.
+    
+    .Parameter Filter
+    A simple filter for the files/folders within the source. Can be used with wildcards.
+    
+    .Parameter LogLocation
+    The path and filename where robocopy logs should be written.
+
+    .Parameter Tee
+    Output to console window, as well as the log file.
+    
+    .Parameter Recurse
+    Copy subdirectories, including Empty ones.
+    
+    .Parameter Open
+    Open destination directory after transfer is completed.
+    
+    .Parameter ExcludeOld
+    Exclude files and folders that older than those in the destination.
+    
+    .Parameter MultiThread
+    Run multiple concurrent transfer threads.
+    
+    .Parameter CopyAll
+    Copy all file and folder attributes.
+    
+    .Parameter RunHours
+    Hour range in which transfers are to be performed.
+    
+    .Parameter PerFile
+    Check run hours between each individual file.
+    
+    .Parameter Retry
+    Number of time to retry a failed file copy before skipping it.
+    
+    .Parameter Mirror
+    Mirror a directory tree.
+    
+    .Parameter Move
+    Delete files from the source.
+    
+    .Parameter List
+    Only list files in source directory. Do not change, copy, delete, or timestamp anything.
+    
+    .Parameter Create
+    Create directory structure and zero length files in destination.
+    
+    .Example
+    Invoke-RoboCopy -Source C:\MyFiles -Destination \\server\user
+    
+    Copy all files from the source folder to the destination folder.
+    
+    .Example
+    Invoke-RoboCopy -Source C:\MyFiles -Destination \\server\user -Filter *work*
+    
+    Copy all files with "work" in the name from the source to the destination.
+    
+    .Example
+    Invoke-RoboCopy -Source C:\MyFiles -Destination \\server\user -RunHour 1900-0600 -PerFile
+    
+    Copy all files between 7:00pm and 6:00am. Check time between every file.
+    
+    .Example
+    Invoke-RoboCopy -Source C:\MyFiles -Destination \\server\user -ExcludeOld
+    
+    Only copy files that are new or have changed.
+    
+    .Example
+    Invoke-RoboCopy -Source C:\MyFiles -Destination \\server\user -List
+    
+    List files that would be copied.
+    
+    .Example
+    Invoke-RoboCopy -Source C:\MyFiles -Destination \\server\user -LogLocation C:\Temp\transfer.log -Tee
+    
+    Copy files while writing a log. Also output log to console.
+    #>
+    param 
+    (
+        [String]
+        [Parameter(Mandatory)]
+        $Source,
+        
+        [String]
+        [Parameter(Mandatory)]
+        $Destination,
+        
+        [String]
+        $Filter = '*',
+        
+        [String]
+        $LogLocation,
+        
+        [Switch]
+        $Tee,
+        
+        [Switch]
+        $Recurse,
+        
+        [Switch]
+        $Open,
+        
+        [Switch]
+        $ExcludeOld,
+        
+        [int]
+        $MultiThread,
+        
+        [Switch]
+        $CopyAll,
+        
+        [String]
+        $RunHours,
+        
+        [Switch]
+        $PerFile,
+        
+        [Int]
+        $Retry = 5,
+        
+        [Switch]
+        $Mirror,
+        
+        [Switch]
+        $Move,
+        
+        [Switch]
+        $List,
+        
+        [Switch]
+        $Create
+    )
+    
+    #**************************
+    # Set various switches here
+    
+    # Recurse, keeping empty directories
+    if ($Recurse)
+    {
+        $DoRecurse = '/E '
+    }
+    else
+    {
+        $DoRecurse = $null
+    }
+    
+    #Exclude old files
+    if ($ExcludeOld)
+    {
+        $DoExclude = '/XO '
+    }
+    else
+    {
+        $DoExclude = $null
+    }
+    
+    #Logging
+    if ($LogLocation)
+    {
+        $DoLogging = "/LOG:`"$LogLocation`" "
+    }
+    else
+    {
+        $DoLogging = $null
+    }
+    
+    #Log Teeing
+    if ($Tee)
+    {
+        $DoTee = '/TEE '
+    }
+    else
+    {
+        $DoTee = $null
+    }
+    
+    #Multithreading
+    if ($MultiThread)
+    {
+        $DoMultithread = "/MT:$MultiThread "
+    }
+    else
+    {
+        $DoMultithread = $null
+    }
+    
+    #Copy all settings
+    if ($CopyAll)
+    {
+        $DoCopyAll = '/COPYALL '
+    }
+    else
+    {
+        $DoCopyAll = $null
+    }
+    
+    #Run hours
+    if ($RunHours)
+    {
+        $DoRunHour = "/RH:`"$RunHours`" "
+    }
+    else
+    {
+        $DoRunHour = $null
+    }
+    
+    #Check run hours per file
+    if ($PerFile)
+    {
+        $DoPerFile = '/PF '
+    }
+    else
+    {
+        $DoPerFile = $null
+    }
+    
+    #Create
+    if ($Create)
+    {
+        $DoCreate = '/CREATE '
+    }
+    else
+    {
+        $DoCreate = $null
+    }
+    
+    #List
+    if ($List)
+    {
+        $DoList = '/L '
+    }
+    else
+    {
+        $DoList = $null
+    }
+    
+    #Move
+    if ($Move)
+    {
+        $DoMove = '/MOVE '
+    }
+    else
+    {
+        $DoMove = $null
+    }
+    
+    #Mirror
+    if ($Mirror)
+    {
+        $DoMirror = '/MIR '
+    }
+    else
+    {
+        $DoMirror = $null
+    }
+    
+    #**************************
+    
+    #Populate arguments string
+    $RoboCopyArgs = '"$Source" '
+    $RoboCopyArgs += '"$Destination" '
+    $RoboCopyArgs += '"$Filter" '
+    $RoboCopyArgs += '$DoRecurse '
+    $RoboCopyArgs += '$DoLogging '
+    $RoboCopyArgs += '$DoTee '
+    $RoboCopyArgs += '$DoExclude '
+    $RoboCopyArgs += '$DoMultithread '
+    $RoboCopyArgs += '$DoCopyAll '
+    $RoboCopyArgs += '$DoRunHour '
+    $RoboCopyArgs += '$DoPerFile '
+    $RoboCopyArgs += '$DoCreate '
+    $RoboCopyArgs += '$DoList '
+    $RoboCopyArgs += '$DoMove '
+    $RoboCopyArgs += '$DoMirror '
+    $RoboCopyArgs += '/R:$Retry'
+    
+    #Expand variable encapsulated in single quotes
+    $RoboCopyArgs = $ExecutionContext.InvokeCommand.ExpandString($RoboCopyArgs)
+    
+    #Begin the robocopy job
+    Start-Process -FilePath 'robocopy.exe' -ArgumentList $RoboCopyArgs -NoNewWindow -Wait
+       
+    if ($Open)
+    {
+        explorer.exe $Destination
+    }
+}
+#endregion
